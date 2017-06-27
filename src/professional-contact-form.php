a523d4764191221aff3700bc306440ad81078f6d
Kelderic <andymercer@outlook.com>
2017-06-27 14:25:30 -0400

    Add Protection Again PHP Versions Below 5.4

--- src/professional-contact-form.php
+++ src/professional-contact-form.php
@@ -34,20 +34,30 @@ require_once dirname( PCF_FILE ) . '/includes/pcf_form.php';
 /*****************************  INITIATE  ******************************/
 /***********************************************************************/
 
-// GENERAL SETUP
+// CONFIRM THAT PHP VERSION IS 5.4 OR HIGHER
 
-add_filter( 'plugin_action_links', function( $links, $file ) {
-	if ( $file == plugin_basename( PCF_FILE ) ) {
-		$links = array_merge($links, array( sprintf( '<a href="%s">%s</a>', admin_url( 'options-general.php?page=pcf_options_page' ), __( 'Settings' ) ) ) );
-	}
-	return $links;
-}, 10, 2 );
+if ( version_compare( PHP_VERSION, '5.4' ) >= 0 ) {
 
-// CLASSES SETUP
+	// GENERAL SETUP
 
-$pcf_enqueuer = new PCF_Enqueuer();
-$pcf_settings = new PCF_Settings();
-$pcf_mailer = new PCF_Mailer();
-$pcf_form = new PCF_Form([
-	'formID' => 'pcf-1',
-]);
+	add_filter( 'plugin_action_links', function( $links, $file ) {
+		if ( $file == plugin_basename( PCF_FILE ) ) {
+			$links = array_merge($links, array( sprintf( '<a href="%s">%s</a>', admin_url( 'options-general.php?page=pcf_options_page' ), __( 'Settings' ) ) ) );
+		}
+		return $links;
+	}, 10, 2 );
+
+	// CLASSES SETUP
+
+	$pcf_enqueuer = new PCF_Enqueuer();
+	$pcf_settings = new PCF_Settings();
+	$pcf_mailer = new PCF_Mailer();
+	$pcf_form = new PCF_Form([
+		'formID' => 'pcf-1',
+	]);
+
+} else {
+
+	add_action( 'admin_notices', create_function( '', "echo '<div class=\"error\"><p>".__('Professional Contact Form requires PHP 5.4 to function properly. Please upgrade PHP or deactivate Plugin Name.', 'professional-contact-form') ."</p></div>';" ) );
+
+}
